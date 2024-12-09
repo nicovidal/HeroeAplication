b8e3086269aa6f376cb3e3679327b260e7dd46ab
nicovidal <nico.vidal.m@gmail.com>
2024-12-09 16:29:04 -0300

    ruta img ok

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -25,7 +25,7 @@ export const HeroPage = () => {
 
     <div className="row mt-5 animate__animated animate__fadeInLeft">
      <div className="col-4">
-        <img src={`/dist/heroes/${id}.jpg`} 
+        <img src={`/heroes/${id}.jpg`} 
         alt={hero.superhero} 
         className="img-thumbnail animate__animated animate__fadeInLeft"/>
      </div>
