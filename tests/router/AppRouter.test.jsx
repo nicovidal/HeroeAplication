b354d03300723bd59de513dca1ed7892764731b0
nicovidal <nico.vidal.m@gmail.com>
2024-12-09 23:14:00 -0300

    mas test

--- tests/router/AppRouter.test.jsx
+++ tests/router/AppRouter.test.jsx
@@ -1,11 +1,52 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { AuthContext } from '../../src/auth';
+import { AppRouter } from '../../src/router/AppRouter';
 
+describe('Pruebas en <AppRouter />', () => {
+    
+    test('debe de mostrar el login si no está autenticado', () => {
 
+        const contextValue = {
+            logged: false,
+        }
 
-describe('Prueba en <AppRouter',()=>{
+        render(
+            <MemoryRouter initialEntries={['/marvel']}>
+                <AuthContext.Provider value={ contextValue }>
+                    <AppRouter />
+                </AuthContext.Provider>
+            </MemoryRouter>
+        );
 
-    test('debe de mostrar el login si no esta autenticado',()=>{
+        expect( screen.getAllByText('Login').length ).toBe(2)
+
         
-    })
+    });
+
+    test('debe de mostrar el componente de Marvel si está autenticado', () => {
+    
+        const contextValue = {
+            logged: true,
+            user: {
+                id: 'ABC',
+                name: 'Juan Carlos'
+            }
+        }
+
+        render(
+            <MemoryRouter initialEntries={['/login']}>
+                <AuthContext.Provider value={ contextValue }>
+                    <AppRouter />
+                </AuthContext.Provider>
+            </MemoryRouter>
+        );
+
+        expect( screen.getAllByText('Marvel').length ).toBeGreaterThanOrEqual(1);
+
+        
+
+    });
 
 
-})
+});
